48050803b775a7b2b3e75a05498f9e4ef2f830e3
Rosheen <rosheenmail@gmail.com>
2014-06-18 16:46:28 -0400

    created basic grid

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,1 +1,28 @@
 /*Javascript file used for Etch-a-Sketch*/
+var rows = prompt("Enter number of rows", 20);
+var cols = prompt("Enter number of columns", 20);
+
+$(document).ready(function(){
+    createGrid();
+});
+
+function createGrid(){
+    /*gives default value if user does not enter anything*/
+    if (rows === null) {
+        rows = 10;
+    }
+    if (cols === null) {
+        cols = 10; 
+    }
+
+   /*creates rows*/
+   for(var i=0; i<rows; i++)
+    {
+        $('tbody').append('<tr></tr>');
+    }
+    /*adds correct number or elements to each row*/
+    for(i=0; i<cols; i++)
+    {
+        $('tr').append('<td></td>');
+    }
+}; 
