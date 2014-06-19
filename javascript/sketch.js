88848ba0f0f0605b7b80ff6d2647497d881b5f2e
Rosheen <rosheenmail@gmail.com>
2014-06-18 22:45:09 -0400

    need to fix random button

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,18 +1,65 @@
 /*Javascript file used for Etch-a-Sketch*/
-//rows = prompt("Enter number of rows", 20);
-//cols = prompt("Enter number of columns", 20);
 var rows = 20;
 var cols = 20;
-
+var col = 'red';
+/* calls function when html is finished downloading*/
 $(document).ready(function()
 {
     createGrid();
-    $(".box").hover(
-    function() {
-        $(this).css('background-color', 'red')
-    });
+    clear();
+    resize();
+    movement(col);
+    random();
 });
 
+/*Picks a random color to when hovering over the box */
+function random() {
+    $('#random').click(function(){
+        color = Math.floor(Math.random()*16777215).toString(16);
+        movement(color);
+    });
+}
+/*clears the markings on the grid and keeps the grid at the same size*/
+function clear()
+{
+      $('#clear').click(function(){
+            $('.box').remove();
+            $('tr').remove();
+            createGrid();
+            movement(col);
+    });
+}
+
+/*asks you dimensions to resize the grid*/
+function resize()
+{
+    $('#size').click(function(){
+        rows = prompt("Enter number of rows", 20);
+        cols = prompt("Enter number of columns", 20);
+        $('.box').remove();
+        $('tr').remove();
+           /*creates rows*/
+        for(var i=0; i<rows; i++)
+        {
+            $('tbody').append('<tr></tr>');
+        }
+        /*adds correct number or elements to each row*/
+        for(i=0; i<cols; i++)
+        {
+            $('tr').append('<td class="box"></td>');
+        }
+        movement(col);
+    });
+}
+/*allows you to move along grid to draw images*/
+function movement(col)
+{
+    $(".box").hover(
+    function() {
+        $(this).css('background-color', col);
+    });
+}
+/*creates the default grid based on the rows and columns*/
 function createGrid()
 {
     /*gives default value if user does not enter anything*/
